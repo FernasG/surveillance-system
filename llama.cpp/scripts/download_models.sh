a5e6685c829de46a246600d8fc4e374d482341ac
FernasG <fernandogoul7@gmail.com>
2026-07-05 00:42:36 -0300

    refactor: update llama.cpp dockerfile messages lang

--- llama.cpp/scripts/download_models.sh
+++ llama.cpp/scripts/download_models.sh
@@ -3,7 +3,7 @@
 apk add --no-cache wget
 
 
-echo "Verificando modelos Qwen..."
+echo "Checking Qwen models..."
 [ -f /models/Qwen3.5-0.8B-Q8_0.gguf ] || wget -O /models/Qwen3.5-0.8B-Q8_0.gguf https://huggingface.co/unsloth/Qwen3.5-0.8B-GGUF/resolve/main/Qwen3.5-0.8B-Q8_0.gguf
 [ -f /models/Qwen3.5-0.8B-mmproj-F16.gguf ] || wget -O /models/Qwen3.5-0.8B-mmproj-F16.gguf https://huggingface.co/unsloth/Qwen3.5-0.8B-GGUF/resolve/main/mmproj-F16.gguf
 
@@ -11,4 +11,4 @@ echo "Verificando modelos Gemma..."
 [ -f /models/gemma-4-E2B-it-qat.Q4_K_M.gguf ] || wget -O /models/gemma-4-E2B-it-qat.Q4_K_M.gguf https://huggingface.co/prithivMLmods/gemma-4-E2B-it-qat-GGUF/resolve/main/gemma-4-E2B-it-qat.Q4_K_M.gguf
 [ -f /models/gemma-4-E2B-it-qat-mmproj-F16.gguf ] || wget -O /models/gemma-4-E2B-it-qat-mmproj-F16.gguf https://huggingface.co/prithivMLmods/gemma-4-E2B-it-qat-GGUF/resolve/main/gemma-4-E2B-it-qat.mmproj-f16.gguf
 
-echo "Todos os modelos prontos!"
+echo "All models are ready!"
